b3ab21c1542cc9460a91d73897a0677fd4ed5293
chessur <riansupit25@yahoo.com>
2020-11-24 01:43:57 +0800

    insertPenelitiAsing tambah : FILE UPLOAD, CALENDAR

---
+++ penelitian/insertPenelitiAsing.php
@@ -0,0 +1,19 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: *');
+
+$dir_berkas = "berkas/";
+$nama_file_smntara = ($_FILES['berkas']['tmp_name']);
+$nama_file = ($_FILES['berkas']['name']);
+$uploadFile = move_uploaded_file($nama_file_smntara, $dir_berkas.$nama_file);
+
+if($uploadFile){
+	echo "upload berhasil ";
+	echo "Link: ".$dir_berkas.$nama_file.$nama_file;
+}else{
+	echo "upload gagal";
+}
+
+
+?>
+
