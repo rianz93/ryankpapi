a01d45642af6e0f03ca82ee901b1fb7815a6ab46
chessur <riansupit25@yahoo.com>
2020-12-30 00:25:35 +0800

    login.php ADDED || table user ADDED

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php 
+	header('Access-Control-Allow-Origin: *');
+	require 'api_conf.php';
+
+	$validateLogin = json_decode($dale -> kueri("SELECT * FROM user WHERE user_nick = '".$_POST['username']."'  AND user_password ='".$_POST['password']."'"));
+	if(sizeof($validateLogin) > 0){
+		$data_pack = array(	'status' 	=> "berhasil", 
+							'id' 	 	=> $validateLogin[0] -> user_id,
+							'nick'		=> $validateLogin[0] -> user_nick,
+						   	'nama' 	 	=> $validateLogin[0] -> user_nama,
+						   	'priority' 	=> $validateLogin[0] -> user_priority);
+		echo json_encode($data_pack);
+
+	}else{
+		echo json_encode(array('status' => "gagal ".$_POST['password']));
+	}
+?>
