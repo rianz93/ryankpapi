823176a41f079c2290bda88ba6e4a11daf809e41
chessur <riansupit25@yahoo.com>
2021-01-02 16:23:10 +0800

    datalist IMPROVED

--- datalist.php
+++ datalist.php
@@ -2,5 +2,14 @@
 	require 'api_conf.php';
 
 	$penulis = json_decode($dale->kueri("SELECT peneliti_nama FROM `peneliti_asing`"));
-	echo json_encode($penulis);
+	$dosen   = json_decode($dale->kueri("SELECT user_nama FROM `user`"));
+
+	$datalist = $penulis;
+	$j = 0;
+	for($i = sizeof($penulis); $i < (sizeof($penulis) + sizeof($dosen)); $i++){
+		$datalist[$i]["peneliti_nama"]  = $dosen[$j] -> user_nama; 
+		$datalist[$i]["number"]			= $i+1;
+		$j++;
+	}
+	echo json_encode($datalist);
  ?>
